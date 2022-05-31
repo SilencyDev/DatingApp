bef5688d66eb766d5cd6d0f9836985719c48cda7
Kevin Macquet <kvn.macquet@gmail.com>
2022-05-31 16:20:02 +0200

    Mid-deployment commit (wip)

--- client/src/app/_interceptor/loading.interceptor.ts
+++ client/src/app/_interceptor/loading.interceptor.ts
@@ -16,7 +16,6 @@ export class LoadingInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.loading.show();
     return next.handle(request).pipe(
-      delay(1000),
       finalize(() => this.loading.hide())
     );
   }
